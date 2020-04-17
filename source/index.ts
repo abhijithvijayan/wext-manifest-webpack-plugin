cba92e8f8ef407e049fd45e466ecd437f75fa9c3
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-17 13:07:11 +0530

    fix: apply new linting rules

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
-import { WextManifestWebpackPlugin } from './plugin';
+import {WextManifestWebpackPlugin} from './plugin';
 
 export default WextManifestWebpackPlugin;
 
--- source/plugin.ts
+++ source/plugin.ts
@@ -3,7 +3,7 @@
  *  @author abhijithvijayan <abhijithvijayan.in>
  */
 
-import { Compiler, compilation as compilationType } from 'webpack';
+import {Compiler, compilation as compilationType} from 'webpack';
 
 const PLUGIN_NAME = 'wext-manifest-webpack-plugin';
 
@@ -12,55 +12,63 @@ type Module = compilationType.Module;
 type Chunk = compilationType.Chunk;
 
 interface CompilationModule extends Module {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	resource?: string | any[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  resource?: string | any[];
 }
 
-function getEntryResource(module: CompilationModule | undefined): string | null {
-	const resource: string | null = null;
+function getEntryResource(
+  module: CompilationModule | undefined
+): string | null {
+  const resource: string | null = null;
 
-	if (module && typeof module.resource === 'string') {
-		return module.resource;
-	}
+  if (module && typeof module.resource === 'string') {
+    return module.resource;
+  }
 
-	return resource;
+  return resource;
 }
 
 export class WextManifestWebpackPlugin {
-	// Define `apply` as its prototype method which is supplied with compiler as its argument
-	apply(compiler: Compiler): void {
-		/**
-		 *  webpack 4+ comes with a new plugin system.
-		 *
-		 *  (// ToDo: support old plugin system //)
-		 */
-		const { hooks } = compiler;
+  // Define `apply` as its prototype method which is supplied with compiler as its argument
+  apply(compiler: Compiler): void {
+    /**
+     *  webpack 4+ comes with a new plugin system.
+     *
+     *  (// ToDo: support old plugin system //)
+     */
+    const {hooks} = compiler;
 
-		// Check for hooks for 4+
-		if (hooks) {
-			// Runs plugin after a compilation has been created.
-			hooks.compilation.tap(PLUGIN_NAME, (compilation: Compilation) => {
-				// Triggered when an asset from a chunk was added to the compilation.
-				compilation.hooks.chunkAsset.tap(PLUGIN_NAME, (chunk: Chunk, file: string) => {
-					// Only handle js files with entry modules
-					if (!file.endsWith('.js') || !chunk.hasEntryModule()) {
-						return;
-					}
+    // Check for hooks for 4+
+    if (hooks) {
+      // Runs plugin after a compilation has been created.
+      hooks.compilation.tap(PLUGIN_NAME, (compilation: Compilation) => {
+        // Triggered when an asset from a chunk was added to the compilation.
+        compilation.hooks.chunkAsset.tap(
+          PLUGIN_NAME,
+          (chunk: Chunk, file: string) => {
+            // Only handle js files with entry modules
+            if (!file.endsWith('.js') || !chunk.hasEntryModule()) {
+              return;
+            }
 
-					// Returns path containing name of asset
-					const resource: null | string = getEntryResource(chunk.entryModule);
-					const isManifest: boolean = (resource && /manifest\.json$/.test(resource)) || false;
+            // Returns path containing name of asset
+            const resource: null | string = getEntryResource(chunk.entryModule);
+            const isManifest: boolean =
+              (resource && /manifest\.json$/.test(resource)) || false;
 
-					if (isManifest) {
-						chunk.files = chunk.files.filter((f: string): boolean => {
-							return f !== file;
-						});
+            if (isManifest) {
+              chunk.files = chunk.files.filter((f: string): boolean => {
+                return f !== file;
+              });
 
-						delete compilation.assets[file];
-						console.warn(`${PLUGIN_NAME}: Removed ${file} from manifest module`);
-					}
-				});
-			});
-		}
-	}
+              delete compilation.assets[file];
+              console.warn(
+                `${PLUGIN_NAME}: Removed ${file} from manifest module`
+              );
+            }
+          }
+        );
+      });
+    }
+  }
 }
