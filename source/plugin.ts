8c617315e58edbb9a2f29e39a90ebc6ff794c5af
Abhijith Vijayan [KUBRIC] <abhijith.v@kubric.io>
2024-01-31 02:04:21 +0530

    feat: webpack 5 support

--- source/plugin.ts
+++ source/plugin.ts
@@ -7,69 +7,121 @@
  *  @license  MIT License
  */
 
-import {Compiler, compilation as compilationType} from 'webpack';
+import {Compiler, Compilation, Module, Dependency} from 'webpack';
 
 const PLUGIN_NAME = 'wext-manifest-webpack-plugin';
-
-type Compilation = compilationType.Compilation;
-type Module = compilationType.Module;
-type Chunk = compilationType.Chunk;
 
 interface CompilationModule extends Module {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   resource?: string | any[];
 }
 
-function getEntryResource(
-  module: CompilationModule | undefined
-): string | null {
-  const resource: string | null = null;
+function getEntryResources(module: CompilationModule | undefined): string[] {
+  const resources: Array<string> = [];
 
   if (module && typeof module.resource === 'string') {
-    return module.resource;
+    resources.push(module.resource);
   }
 
-  return resource;
+  return resources;
+}
+
+function collectEntryResources(
+  compilation: Compilation,
+  module: CompilationModule,
+  cache: Array<ReturnType<typeof getEntryResources>>
+): ReturnType<typeof getEntryResources> {
+  const index = compilation.moduleGraph.getPreOrderIndex(module as never) ?? -1;
+
+  // index of module is unique per compilation
+  // module.id can be null, not used here
+  if (cache[index] !== undefined) {
+    return cache[index];
+  }
+
+  if (module && typeof module.resource === 'string') {
+    cache[index] = getEntryResources(module);
+    return cache[index];
+  }
+
+  const resources: ReturnType<typeof getEntryResources> = [];
+  if (module && module.dependencies) {
+    const hasModuleGraphSupport = Object.prototype.hasOwnProperty.call(
+      compilation,
+      'moduleGraph'
+    );
+    module.dependencies.forEach((dep) => {
+      if (dep) {
+        const depModule = hasModuleGraphSupport
+          ? compilation.moduleGraph.getModule(dep)
+          : dep.module;
+        const originModule = hasModuleGraphSupport
+          ? compilation.moduleGraph.getParentModule(dep)
+          : (dep as Dependency & {originModule: Module}).originModule;
+        const nextModule = depModule || originModule;
+        if (nextModule) {
+          const depResources = collectEntryResources(
+            compilation,
+            nextModule,
+            cache
+          );
+          for (let i = 0, {length} = depResources; i !== length; i += 1) {
+            resources.push(depResources[i]);
+          }
+        }
+      }
+    });
+  }
+
+  cache[index] = resources;
+
+  return resources;
 }
 
 export class WextManifestWebpackPlugin {
   // Define `apply` as its prototype method which is supplied with compiler as its argument
   apply(compiler: Compiler): void {
-    /**
-     *  webpack 4+ comes with a new plugin system.
-     *
-     *  (// ToDo: support old plugin system //)
-     */
     const {hooks} = compiler;
-
-    // Check for hooks for 4+
     if (hooks) {
       // Runs plugin after a compilation has been created.
-      hooks.compilation.tap(PLUGIN_NAME, (compilation: Compilation) => {
+      hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
+        const resourcesCache: Array<ReturnType<typeof getEntryResources>> = [];
+
         // Triggered when an asset from a chunk was added to the compilation.
-        compilation.hooks.chunkAsset.tap(
-          PLUGIN_NAME,
-          (chunk: Chunk, file: string) => {
-            // Only handle js files with entry modules
-            if (!file.endsWith('.js') || !chunk.hasEntryModule()) {
-              return;
-            }
-
-            // Returns path containing name of asset
-            const resource: null | string = getEntryResource(chunk.entryModule);
-            const isManifest: boolean =
-              (resource && /manifest\.json$/.test(resource)) || false;
-
-            if (isManifest) {
-              chunk.files = chunk.files.filter((f: string): boolean => {
-                return f !== file;
-              });
-
-              delete compilation.assets[file];
-              console.log(`${PLUGIN_NAME}: removed ${file}`);
-            }
+        compilation.hooks.chunkAsset.tap(PLUGIN_NAME, (chunk, file: string) => {
+          // Only handle js files with entry modules
+          if (
+            !file.endsWith('.js') ||
+            compilation.chunkGraph.getNumberOfEntryModules(chunk) < 1
+          ) {
+            return;
           }
-        );
+
+          const entryModules: Array<Module> = Array.from(
+            compilation.chunkGraph.getChunkEntryModulesIterable(chunk)
+          );
+          if (entryModules.length < 1) {
+            return;
+          }
+
+          const entryModule = entryModules[0];
+          const resources = collectEntryResources(
+            compilation,
+            entryModule,
+            resourcesCache
+          );
+
+          const isManifest: boolean = resources.every(
+            (resource) => resource && /manifest\.json$/.test(resource)
+          );
+
+          if (isManifest) {
+            chunk.files.delete(file);
+
+            delete compilation.assets[file];
+            console.log(`${PLUGIN_NAME}: removed ${file}`);
+          }
+        });
       });
     }
   }
